a92399655ab5e80bcc08700c6885737c0f9bf693
binauralbeat <binauralbeat001@gmail.com>
2018-04-12 11:48:51 -0500

    attempt at styling

---
+++ exec.js
@@ -0,0 +1,39 @@
+// Student database creation
+const Aliases = {}
+
+// Student table in database
+Aliases.names = []
+
+// Add names to the student table in the student database
+Aliases.names.push(
+   {
+       name: “Jacob Smith”,
+       birthPlace: “El Paso, TX”,
+       gender: “M”
+   },
+   {
+       name: “Rachael Babcock”,
+       birthPlace: “Tulsa, OK”,
+       gender: “F”
+   },
+   {
+       name: “Daniel Beecroft”,
+       birthPlace: “Kingston Springs, TN”,
+       gender: “M”
+   },
+   {
+       name: “Deanna Vickers”,
+       birthPlace: “Dollar bay, MI”,
+       gender: “F”
+   },
+   {
+       name: “Hayley Landsberg”,
+       birthPlace: “Coral Springs, FL”,
+       gender: “F”
+   },
+   {
+       name: “Ronnie Young”,
+       birthPlace: “Middlesboro, KY”,
+       gender: “M”
+   }
+)
