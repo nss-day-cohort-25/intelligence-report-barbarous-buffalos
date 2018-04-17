420aefbe5497ce4bd398c0a7174c275118543470
Meghan Debity <mdebity@gmail.com>
2018-04-17 13:51:59 -0500

    completed array, working on function

--- finInt/finInt.js
+++ finInt/finInt.js
@@ -1,50 +1,26 @@
 //Use querySelector()
 const financialBodyRef = document.querySelector("financialInterests")
 
-//Write to DOM
-//content array
-const bankrollArray = [
-    {
-        bank: "Suspected of receiving funding from 'hacktivists'"
-    },
-    {
-        bank: "Paid speaking engagements"
-    },
-    {
-        bank: "Unconfirmed support from the Russian Federation"
+//Send to DOM
+FinancialDatabase.details.forEach(
+    currentDetails => {
+        //create section elements
+    const list = document.createElement('section')
+    const listItems = document.createAttribute('class')
+    listItems.value = 'financialDetails' //section class="financialDetails"
+
+        //Add the attribute to the component
+        list.setAttributeNode(listItems)
+
+        //h2
+       const titleHead = document.createElement('h2')
+       titleHead.value = FinancialDatabase.details.header()
+
+        console.log(titleHead)
+        //h3
+            //ul
+                //li
     }
-]
-//create section elements
-const list = document.createElement('section')
-const listItems = document.createAttribute('class')
-listItems.value = 'financialSources' //section class="financialSources"
-    //Add the attribute to the component
-    list.setAttributeNode(listItems)
+)
 
-    //h2
-    const sourceHeader = document.createElement('h2')
-    sourceHeader.textContent("Potential Bankroll Sources")
 
-    list.appendChild(sourceHeader)
-    //h3
-        //ul
-            //li
-//financialDatabase.js for arrays and objects -- local storage
-
-{
-    name:"header title"
-    infoContent: {
-        li
-    },
-    {
-        li
-    }
-},
-{
-    name: "header title"
-    infoContent: "paragraph content"
-},
-{
-    name: "header title"
-    infoContent: "paragraph content"
-}
